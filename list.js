481814c0e833cb9334c2f343e9a0709f5d5abac2
Cody Gillette <cgillette1312@gmail.com>
2019-06-13 13:05:11 -0700

    update

--- list.js
+++ list.js
@@ -18,95 +18,95 @@ class LinkedList {
         this.head = new _Node(item, this.head);
     }
 
-    insertLast(item){
+    insertLast(item) {
         let tempNode = this.head;
-        while(tempNode.next !== null){
+        while (tempNode.next !== null) {
             tempNode = tempNode.next;
         }
-        tempNode.next = new _Node(item, null); 
+        tempNode.next = new _Node(item, null);
     }
-    insertBefore(item, target){
+    insertBefore(item, target) {
         let currNode = this.head;
-        if(!this.head){
+        if (!this.head) {
             return null;
         }
-        while(currNode.next.value !== target ){
-            if (currNode.next.value === null){
+        while (currNode.next.value !== target) {
+            if (currNode.next.value === null) {
                 return null;
             }
-            else{
+            else {
                 currNode = currNode.next;
             }
         }
         currNode.next = new _Node(item, currNode.next);
-        
+
     }
-    insertAfter(item, target){
+    insertAfter(item, target) {
         let currNode = this.head;
-        if(!this.head){
+        if (!this.head) {
             return null;
         }
-        while(currNode.value !== target){
-            if(currNode.next === null){
+        while (currNode.value !== target) {
+            if (currNode.next === null) {
                 return null;
             }
-            else{
+            else {
                 currNode = currNode.next;
             }
         }
         currNode.next = new _Node(item, currNode.next);
     }
-    insertAt(item, pos){
-        let i = 0 ;
+    insertAt(item, pos) {
+        let i = 0;
         let currNode = this.head;
-        
-        if(pos === 0){
+
+        if (pos === 0) {
             this.insertFirst(item);
         }
-        if(!this.head){
+        if (!this.head) {
             return null;
         }
-        while( i < pos ){
+        while (i < pos) {
 
             currNode = currNode.next;
             i++;
 
-            }
+        }
         this.insertBefore(item, currNode.value);
-        
+
     }
 
-    find(item){
+    find(item) {
         let currNode = this.head;
 
-        if (!this.head){
+        if (!this.head) {
             return null;
         }
-        while (currNode.value !== item){
-            if(currNode.next === null){
+        while (currNode.value !== item) {
+            if (currNode.next === null) {
                 return null;
             }
-            else{
+            else {
                 currNode = currNode.next;
             }
         }
         return currNode;
     }
-    remove(item){
-        if(!this.head){
+    remove(item) {
+        if (!this.head) {
             return null;
         }
 
-        if(this.head.value === item) {
+        if (this.head.value === item) {
             this.head = this.head.next;
         }
         let currNode = this.head;
         let previousNode = this.head;
-        while((currNode !== null) && (currNode.value !== item)){
+        while ((currNode !== null) && (currNode.value !== item)) {
             previousNode = currNode;
             currNode = currNode.next;
         }
-        if(currNode === null){
+        if (currNode === null) {
             console.log('item not found');
             return;
         }
@@ -124,7 +124,7 @@ ll.insertFirst('stuff');
 
 // console.log(ll);
 
-function main(){
+function main() {
     let SLL = new LinkedList();
     SLL.insertFirst('Apollo');
     SLL.insertLast('Boomer');
@@ -139,7 +139,7 @@ function main(){
     SLL.remove('Tauhida');
 }
 // display: displays the linked list
-function display(list){
+function display(list) {
     let values = '';
     let currNode = list.head;
     if (!list.head) {
@@ -147,18 +147,18 @@ function display(list){
     }
     while (currNode.next !== null) {
 
-        values+= currNode.value;
+        values += currNode.value;
         currNode = currNode.next;
     }
-    values+= currNode.value;
-    console.log( values);
+    values += currNode.value;
+    console.log(values);
 
 }
 // console.log(display(ll));
 
 // size: returns the size of the linked list
-function size(list){
-    let length= 1;
+function size(list) {
+    let length = 1;
     let currNode = list.head;
     if (!list.head) {
         return null;
@@ -171,50 +171,80 @@ function size(list){
     console.log(length);
 }
 
-console.log(size(ll));
+// console.log(size(ll));
 // isEmpty: finds if the list is empty or not(without using the size() function)
-function isEmpty(list){
-    if(!list.head){
+function isEmpty(list) {
+    if (!list.head) {
         return;
-    }else{
+    } else {
         return true;
     }
 }
-console.log(isEmpty(ll));
+// console.log(isEmpty(ll));
 // findPrevious: finds the node before the item you are looking for
-function findPrevious(list , item){
+function findPrevious(list, item) {
     let currNode = list.head;
-    while(currNode.next.value !== item){
+    while (currNode.next.value !== item) {
         currNode = currNode.next;
-        
+
     }
     console.log(currNode.value);
 }
 // console.log(findPrevious(ll, 'other' ));
 
 // findLast: returns the last node in the linked list
-function findLast(list){
+function findLast(list) {
     let currNode = list.head;
-    while(currNode.next !== null){
+    while (currNode.next !== null) {
         currNode = currNode.next;
     }
     console.log(currNode.value);
 }
-console.log(findLast(ll));
+// console.log(findLast(ll));
 
 class DLinkedList {
-    constructor(){
+    constructor() {
         this.head = null;
         this.tail = null;
     }
-    insertFirstDll(item){
+    insertFirstDll(item) {
         let node = new _Node(item, this.head, null);
-            if(this.head !== null){
-                this.head.prev = node;
-            }
-            this.head = node;
-            if(this.tail === null){
-                this.tail = node;
-            }
+        if (this.head !== null) {
+            this.head.prev = node;
+        }
+        this.head = node;
+        if (this.tail === null) {
+            this.tail = node;
+        }
     }
 }
+// 4. mistey driver 
+function deleteDup(lst) {
+    let current = lst.head;
+    while (current !== null) {
+        let newNode = current;
+        while (newNode.next !== null) {
+            if (newNode.next.value === current.value) {
+                newNode.next = newNode.next.next;
+            }
+            else { newNode = newNode.next; }
+        }
+        current = current.next;
+    }
+}
+// getting ride of duplicates; O(n);
+
+// 5 
+function reverse(list) {
+
+    let currNode = list.head;
+
+    while (currNode.next !== null) {
+        list.insertFirst(currNode.next.value);
+        currNode = currNode.next;
+    }
+    deleteDup(list);
+
+}
+reverse(ll);
+console.log(display(ll));
